70eb1b94e9fd535410bcf56e2d04729ece25a794
girausquin <gabriel.irausquin.jordan@gmail.com>
2025-11-23 20:52:49 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./next-intl.config.mjs');
 
 const nextConfig = {
-  output: 'export',
   images: { unoptimized: true },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true }
 };
 
 export default withNextIntl(nextConfig);
+
